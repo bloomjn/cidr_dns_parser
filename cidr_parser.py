f0f04cfdccbacd0fbb637fe64736426f732925e7
bloomjn <47399654+bloomjn@users.noreply.github.com>
2023-02-28 16:01:42 -0500

    Update cidr_parser.py

--- cidr_parser.py
+++ cidr_parser.py
@@ -1,31 +1,41 @@
-import json, ipaddress, requests, argparse, dns.resolver
+import json, ipaddress, requests, argparse
 
 #Requires dnspython to be installed
-#pip3 install dnspython
+try: 
+    import dns.resolver
+except ImportError: 
+    print("This script requires dnspython modules")
+    print("pip3 install dnspython")
+    raise SystemExit
 
 #Pass Hostname Argument through CLI
 CLI=argparse.ArgumentParser()
-CLI.add_argument("--host", type=str)
+CLI.add_argument('--list', nargs='+', required=True)
 args=CLI.parse_args()
-hostname=args.host
+hostname_list=[]
+hostname_list=args.list
 
-#Return A Record 20 Times. Run a new DNS query every time to avoid cache lookups.
+#Return A Record 50 Times. Run a new DNS query every time to avoid cache lookups.
 list=[]
-count=0
-while count<20:
-    resolver=dns.resolver.Resolver(configure=False)
-    resolver.nameservers = ['1.1.1.1','8.8.8.8']
-    answer=resolver.resolve('auth.us-ashburn-1.oraclecloud.com', 'a')
-    for ipval in answer:
-        ipaddr=(ipval.to_text())
-        list.append(ipaddr)
-    count=count+1
+for hostname in hostname_list:
+    count=0
+    print(hostname)
+    while count<50:
+        print(hostname)
+        resolver=dns.resolver.Resolver(configure=False)
+        resolver.nameservers = ['1.1.1.1','8.8.8.8']
+        answer=resolver.resolve(hostname, 'a')
+        for ipval in answer:
+            ipaddr=(ipval.to_text())
+            list.append(ipaddr)
+        count=count+1
 
 #Iterate Through List of IPs, and return Unique IPs
 unique_ip_list=[]
 for x in list:
     if x not in unique_ip_list:
         unique_ip_list.append(x)
+print("")
 print("Unique IP's From DNS A Records")
 print(unique_ip_list)
 
@@ -48,7 +58,9 @@ for ip_addr in unique_ip_list:
     for region in cidrs:
         for c in region['cidrs']:
             if ipaddress.ip_address(ip_addr) in ipaddress.ip_network(c['cidr']):
-                cidr_found=(c['cidr'], region['region'])
+                #cidr_found=("{}-{}").format(region['region'], c['cidr'])
+                #Uncomment this line if you want to see the region for each prefix.
+                cidr_found=("{}").format(c['cidr'])
                 cidr_blocks.append(cidr_found)
                 cidr_blocks.append(cidr_found)
 
@@ -56,5 +68,7 @@ unique_cidr_blocks=[]
 for x in cidr_blocks:
     if x not in unique_cidr_blocks:
         unique_cidr_blocks.append(x)
-print("Unique CIDR BLOCKS")
+print("")
+print("Unique CIDR BLOCKS matched with OCI JSON file")
 print(unique_cidr_blocks)
+print("There are {} unique CIDRs in this list.".format(len(unique_cidr_blocks)))
